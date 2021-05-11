f93814a2658bd84e1265c12268ab151cde460073
Clay Risser <jam@codejam.ninja>
2021-05-11 13:28:07 -0500

    Update blackmagic.mk

--- blackmagic.mk
+++ blackmagic.mk
@@ -15,44 +15,62 @@
 #
 # - Clay Risser
 
-PLATFORM := $(shell node -e "process.stdout.write(process.platform)")
-
-ifeq ($(PLATFORM),win32)
-  BANG ?= !
-	MAKE ?= make
-	NULL ?= nul
-	SHELL ?= cmd.exe
-	GREP ?= grep
-	SED ?= sed
-else
-	BANG ?= \!
-	NULL ?= /dev/null
-	SHELL ?= $(shell bash --version >$(NULL) 2>&1 && echo bash|| echo sh)
-ifeq ($(PLATFORM),darwin)
-	GREP ?= ggrep
-	SED ?= gsed
-else
-	GREP ?= grep
-	SED ?= sed
-endif
-endif
-ifeq ($(PLATFORM),linux)
-	NUMPROC ?= $(shell grep -c ^processor /proc/cpuinfo)
-endif
-ifeq ($(PLATFORM),darwin)
-	NUMPROC ?= $(shell sysctl hw.ncpu | awk '{print $$2}')
-endif
-export NUMPROC ?= 1
-MAKEFLAGS += "-j $(NUMPROC)"
-
 CWD := $(shell pwd)
-export CD ?= cd
-export GIT ?= $(shell git --version >$(NULL) 2>&1 && echo git|| echo true)
-export NOFAIL := 2>$(NULL)|| true
+export NO_CHECK ?= false
+export MAKE_CACHE ?= $(CWD)/.make
+export _ACTIONS := $(MAKE_CACHE)/actions
+export CHECK := $(MAKE_CACHE)/check
+export DEPS := $(MAKE_CACHE)/deps
+export DONE := $(MAKE_CACHE)/done
+export ENVS := $(MAKE_CACHE)/envs
+export ACTION := $(DONE)
 
 .EXPORT_ALL_VARIABLES:
 
-export PROJECT_ROOT ?= $(shell $(GIT) rev-parse --show-superproject-working-tree)
+-include $(ENVS)
+
+export PLATFORM := $(shell node -e "process.stdout.write(process.platform)")
+export NIX_ENV := $(shell which sed | grep -qE "^/nix/store" && echo true|| echo false)
+ifeq ($(PLATFORM),win32)
+	BANG := !
+	MAKE := make
+	NULL := nul
+	SHELL := cmd.exe
+else
+	BANG := \!
+	NULL := /dev/null
+	SHELL := $(shell bash --version >$(NULL) 2>&1 && echo bash|| echo sh)
+endif
+ifeq ($(NIX_ENV),true)
+	export GREP ?= grep
+	export SED ?= sed
+else
+ifeq ($(PLATFORM),darwin)
+	export GREP ?= ggrep
+	export SED ?= gsed
+else
+	export GREP ?= grep
+	export SED ?= sed
+endif
+endif
+ifeq ($(PLATFORM),linux)
+	export NUMPROC ?= $(shell grep -c ^processor /proc/cpuinfo)
+	export OPEN ?= xdg-open
+else
+	export OPEN ?= open
+endif
+ifeq ($(PLATFORM),darwin)
+	export NUMPROC ?= $(shell sysctl hw.ncpu | awk '{print $$2}')
+endif
+export NUMPROC ?= 1
+# MAKEFLAGS += "-j $(NUMPROC)"
+
+export CD ?= cd
+export GIT ?= $(shell git --version >$(NULL) 2>&1 && echo git|| echo true)
+export NPM ?= $(shell pnpm --version >$(NULL) 2>&1 && echo pnpm|| (yarn --version >$(NULL) 2>&1 && echo yarn|| echo npm))
+export NOFAIL := 2>$(NULL)|| true
+
+PROJECT_ROOT ?= $(shell $(GIT) rev-parse --show-superproject-working-tree)
 ifeq ($(PROJECT_ROOT),)
 	PROJECT_ROOT := $(shell $(GIT) rev-parse --show-toplevel)
 endif
@@ -67,17 +85,14 @@ ifeq ($(PARENT),true)
 endif
 endif
 
-export MAKE_CACHE ?= $(CWD)/.make
-export _ACTIONS := $(MAKE_CACHE)/actions
-export DONE := $(MAKE_CACHE)/done
-export DEPS := $(MAKE_CACHE)/deps
-export ACTION := $(DONE)
+.EXPORT_ALL_VARIABLES:
 
 _RUN := $(shell mkdir -p $(_ACTIONS) $(DEPS) $(DONE))
 
 define done
 	$(call reset_deps,$1)
 	touch -m $(DONE)/$1
+	rm -f $(DONE)/+$1 $(NOFAIL)
 endef
 
 define add_dep
@@ -108,17 +123,25 @@ define clean
 	rm -rf $(MAKE_CACHE) $(NOFAIL)
 endef
 
+define reset_envs
+	rm -rf $(ENVS) $(NOFAIL)
+endef
+
 define ACTION_TEMPLATE
 ifeq ($$(CHILD),true)
 ifneq ($$(CHILD_{{ACTION_UPPER}}_READY),true)
 CHILD_{{ACTION_UPPER}}_READY = true
 .PHONY: child_{{ACTION}} child_+{{ACTION}} child__{{ACTION}} child_~{{ACTION}}
 child_{{ACTION}}: child__{{ACTION}} child_~{{ACTION}}
-child_~{{ACTION}}: child_{{ACTION_DEPENDENCY}} $$({{ACTION_UPPER}}_TARGET)
-child_+{{ACTION}}: child__{{ACTION}} $$({{ACTION_UPPER}}_TARGET)
+child_~{{ACTION}}: | $$(CHECK) {{CHILD_ACTION_DEPENDENCY}} $$({{ACTION_UPPER}}_DEPS) \
+	$$({{ACTION_UPPER}}_TARGET) $$(ACTION)/{{ACTION}}
+child_+{{ACTION}}: | $$(CHECK) child__{{ACTION}} $$({{ACTION_UPPER}}_DEPS) \
+	$$({{ACTION_UPPER}}_TARGET) $$(ACTION)/{{ACTION}}
 child__{{ACTION}}:
+	@touch -m $$(DONE)/+{{ACTION}}
+	@$$(call clear_cache,$$(DONE)/_{{ACTION}})
 	@$$(call clear_cache,$$(DONE)/{{ACTION}})
-$$(DONE)/_{{ACTION}}/%: %
+$$(DONE)/_{{ACTION}}/*: %
 	@$$(call clear_cache,$$(DONE)/{{ACTION}})
 	@$$(call add_dep,{{ACTION}},$$<)
 	@$$(call cache,$$@)
@@ -128,11 +151,15 @@ ifneq ($$({{ACTION_UPPER}}_READY),true)
 {{ACTION_UPPER}}_READY = true
 .PHONY: {{ACTION}} +{{ACTION}} _{{ACTION}} ~{{ACTION}}
 {{ACTION}}: _{{ACTION}} ~{{ACTION}}
-~{{ACTION}}: {{ACTION_DEPENDENCY}} $$({{ACTION_UPPER}}_TARGET)
-+{{ACTION}}: _{{ACTION}} $$({{ACTION_UPPER}}_TARGET)
+~{{ACTION}}: | $$(CHECK) {{ACTION_DEPENDENCY}} $$({{ACTION_UPPER}}_DEPS) \
+	$$({{ACTION_UPPER}}_TARGET) $$(ACTION)/{{ACTION}}
++{{ACTION}}: | $$(CHECK) _{{ACTION}} $$({{ACTION_UPPER}}_DEPS) \
+	$$({{ACTION_UPPER}}_TARGET) $$(ACTION)/{{ACTION}}
 _{{ACTION}}:
+	@touch -m $$(DONE)/+{{ACTION}}
+	@$$(call clear_cache,$$(DONE)/_{{ACTION}})
 	@$$(call clear_cache,$$(DONE)/{{ACTION}})
-$$(DONE)/_{{ACTION}}/%: %
+$$(DONE)/_{{ACTION}}/*: %
 	@$$(call clear_cache,$$(DONE)/{{ACTION}})
 	@$$(call add_dep,{{ACTION}},$$<)
 	@$$(call cache,$$@)
@@ -145,7 +172,35 @@ $(_ACTIONS)/%:
 	@ACTION_BLOCK=$(shell echo $@ | $(GREP) -oE '[^\/]+$$') && \
 		ACTION=$$(echo $$ACTION_BLOCK | $(GREP) -oE '^[^~]+') && \
 		ACTION_DEPENDENCY=$$(echo $$ACTION_BLOCK | $(GREP) -oE '~[^~]+$$' $(NOFAIL)) && \
+		CHILD_ACTION_DEPENDENCY=$$([ "$$ACTION_DEPENDENCY" = "" ] && echo ""|| echo "child_$$ACTION_DEPENDENCY") && \
 		ACTION_UPPER=$$(echo $$ACTION | tr '[:lower:]' '[:upper:]') && \
 		echo "$${ACTION_TEMPLATE}" | $(SED) "s/{{ACTION}}/$${ACTION}/g" | \
 		$(SED) "s/{{ACTION_DEPENDENCY}}/$${ACTION_DEPENDENCY}/g" | \
+		$(SED) "s/{{CHILD_ACTION_DEPENDENCY}}/$${CHILD_ACTION_DEPENDENCY}/g" | \
 		$(SED) "s/{{ACTION_UPPER}}/$${ACTION_UPPER}/g" > $@
+
+$(CHECK): Makefile
+ifneq ($(NO_CHECK),true)
+	@echo "🔌 checking dependencies"
+	@echo "💣 busted cache"
+	@rm -rf $(MAKE_CACHE)
+endif
+	@mkdir -p $(_ACTIONS) $(DEPS) $(DONE)
+	@touch -m $(CHECK)
+
+$(ENVS): Makefile
+	@echo "🗲  make will be faster next time"
+	@rm -rf $@
+	@for e in $$CACHE_ENVS; do \
+		echo "export $$e := $$(eval "echo \$$$$e")" >> $@; \
+	done
+
+CACHE_ENVS += \
+	GIT \
+	GREP \
+	NIX_ENV \
+	NPM \
+	NUMPROC \
+	PLATFORM \
+	PROJECT_ROOT \
+	SED
